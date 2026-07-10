a7ec86b79ebcf5ec3bfa7cf0de827c1f74052b80
Anaverica <anastasiu.veronika@gmail.com>
2026-07-10 14:25:32 +0300

    Изменен файл

--- ex_debug.py
+++ ex_debug.py
@@ -1,19 +1,24 @@
 class Vehicle:
-    def __init__(self):
+    def __init__(self, color,engine, breaks):
         self.speed = 0
-       # self.color = "white"
+        self.color = color
+        self.engine = engine
+        self.breaks = breaks
 
     def move(self):
         print(f"Машина двигается со скоростью {self.speed}")
 
     def info(self):
         print(f"Цвет машины {self.color}")
-
+        print(f"Двигатель {self.engine}")
+        print(f"Торомоза {self.breaks}")
+        
 
 class Car(Vehicle):
-    def __init__(self):
+    def __init__(self,color,engine, breaks):
+        super().__init__(color,engine, breaks)
         self.speed = 100
 
-car = Car()
+car = Car("Голубой", 1500, "vip_strong")
 car.move()
-#car.info()
+car.info()
